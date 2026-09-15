7b5ad25ed5a3ce3d5a245cbfd0065f1fb98f02a6
Timothy Cullin <timcullin@Mac.modem>
2026-09-15 12:07:50 +1000

    Update footer acknowledgment

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -57,10 +57,10 @@ export default function Footer() {
                 />
 
                 <p className={styles.acknowledgment}>
-                    Established in Victoria, Australia, Sesame Legal is
-                    informed by an appreciation for Victoria’s civic
-                    traditions, Australia’s democratic institutions,
-                    and the administration of justice.
+                    Sesame Legal acknowledges Aboriginal and Torres Strait
+                    Islander peoples as the first inhabitants and the
+                    Traditional Custodians of the lands where we live, learn,
+                    work and play.
                 </p>
             </div>
         </footer>
